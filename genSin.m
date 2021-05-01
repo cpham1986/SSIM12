b11e889d011c03a17de99a40efff1687d959c924
cpham1986 <chrispham0900@yahoo.com>
2021-05-01 15:43:57 -0400

    getSin and plotOutput functions
    started implementing said functions

---
+++ genSin.m
@@ -0,0 +1,32 @@
+function v = genSin(bpsk)
+%GENSIN Summary of this function goes here
+%   Detailed explanation goes here
+% CMPE 349 - Project
+% Team 12
+% 4/19/21
+
+AZ  = [0 0 1 0 0 0 1]; % BPSK - 0011001 DPSK - 0010001
+BAZ = [1 1 1 0 0 0 1]; % BPSK - 1001001 DPSK - 1110001
+EL  = [1 0 0 0 0 0 1]; % BPSK - 1100001 DPSK - 1000001
+
+
+
+phaseShift
+
+T 
+
+thetaT
+
+% AmpT is the amplitude constant from the TCU
+AmpT
+
+% Sinc function
+%v(t) = AmpT * ((sin(pi*((thetaT-thetaR)/(1.15*thetaBW))))./(pi*((thetaT-thetaR)/(1.15*thetaBW)))).*sin(omega*T+phaseShift);
+
+x = ((thetaT-thetaR)/(1.15*thetaBW))./(pi*((thetaT-thetaR)/(1.15*thetaBW)));
+
+v = AmpT * sinc(x).*sin(omega*T+phaseShift);
+
+
+end
+
---
+++ plotOutput.m
@@ -0,0 +1,6 @@
+function plotOutput(T, V)
+%PLOTOUTPUT Summary of this function goes here
+%   Detailed explanation goes here
+plot(T,V);
+end
+
