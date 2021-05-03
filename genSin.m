727a4190f0bb7afb0d566bdbc0a379f2b8249047
cpham1986 <chrispham0900@yahoo.com>
2021-05-03 10:20:00 -0400

    updated gensin

--- genSin.m
+++ genSin.m
@@ -9,24 +9,23 @@ AZ  = [0 0 1 0 0 0 1]; % BPSK - 0011001 DPSK - 0010001
 BAZ = [1 1 1 0 0 0 1]; % BPSK - 1001001 DPSK - 1110001
 EL  = [1 0 0 0 0 0 1]; % BPSK - 1100001 DPSK - 1000001
 
+[funct, antenna] = ident(bpsk);
 
 
+[thetaR, thetaBW, thetaMIN, thetaMAX, AmpT] = modVar(funct, antenna)
 phaseShift
 
-T 
-
-thetaT
-
-% AmpT is the amplitude constant from the TCU
-AmpT
+T = 0:1/20:1;
 
 % Sinc function
 %v(t) = AmpT * ((sin(pi*((thetaT-thetaR)/(1.15*thetaBW))))./(pi*((thetaT-thetaR)/(1.15*thetaBW)))).*sin(omega*T+phaseShift);
 
-x = ((thetaT-thetaR)/(1.15*thetaBW))./(pi*((thetaT-thetaR)/(1.15*thetaBW)));
+%mod theta T accordingly
+thetaT = time/ + thetaMax;
 
+x = ((thetaT-thetaR)/(1.15*thetaBW))./(pi*((thetaT-thetaR)/(1.15*thetaBW)));
 v = AmpT * sinc(x).*sin(omega*T+phaseShift);
 
-
+plotOutput(T,v);
 end
 
