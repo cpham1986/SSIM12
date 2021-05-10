2fd3001509c59a6408bbaf0d333623207e41fcfa
cpham1986 <chrispham0900@yahoo.com>
2021-05-09 20:16:29 -0400

    Update modVar.m

--- modVar.m
+++ modVar.m
@@ -8,7 +8,48 @@ function [thetaR, thetaBW, thetaMIN, thetaMAX, A] = modVar(ID, ANT)
 AZ  = 0010001; % BPSK - 0011001
 BAZ = 1110001; % BPSK - 1001001
 EL  = 1000001; % BPSK - 1100001
+BD1;
+BD2;
+BD3;
+BD4;
+BD5;
+BD6;
+AD1;
+AD2;
+AD3;
 
+
+% BD1 = [0,1,0,1,0,0,0]; convert all of these to DPSK and add them
+% BD2 = [0,1,1,1,1,0,0];
+% BD3 = [1,0,1,0,0,0,0];
+% BD4 = [1,0,0,0,1,0,0];
+% BD5 = [1,1,0,1,1,0,0];
+% BD6 = [0,0,0,1,1,0,1];
+% AD1 = [1,1,1,0,0,1,0];
+% AD2 = [1,0,1,0,1,1,1];
+% AD3 = [1,1,1,1,0,0,0];
+
+
+switch ID
+    case BD1
+        ID = AZ;
+    case BD2
+        ID = BAZ;
+    case BD3
+        ID = AZ;
+    case BD4
+        ID = AZ;
+    case BD5
+        ID = AZ;
+    case BD6
+        ID = AZ;
+    case AD1
+        ID = AZ;
+    case AD2
+        ID = AZ;
+    case AD3
+        ID = AZ;        
+    
 switch ID
     case AZ
         thetaR = -12;
