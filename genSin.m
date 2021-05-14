81de43438a8fc224eb3f44289245ffa624a12f9a
cpham1986 <chrispham0900@yahoo.com>
2021-05-14 17:41:18 -0400

    implemented plotoutput
    in theory done, untested

--- genSin.m
+++ genSin.m
@@ -1,41 +1,41 @@
 % function:
 % input: bpsk - the input from TCU
 % output: the sinc output for SSIM (may be arbitrary)
-function v = genSin(data)
+function genSin(data)
 %GENSIN Summary of this function goes here
 %   Detailed explanation goes here
 % CMPE 349 - Project
 % Team 12
 % 4/19/21
-% prevIndex = 1;
+prevIndex = 1;
+
+for i = 1:length(testArr)
+    %code for 1 signal
+    if mod(d, 5) == 0
+        index = i-1;
+        %should grab all columns
+        data2 = data(prevIndex:index, :);
+        time = prevIndex:index;
+        plotOutput(time, data2);
+        prevIndex = index;
+        
+    elseif data(i,1) ~= data(i+1,1)
+        d = d + 1;
+    end
+end
+
+if(index < testArr)
+%add code to plot last thing if there is one
+end
+
+% AZ  = [0 0 1 0 0 0 1]; % BPSK - 0011001 DPSK - 0010001
+% BAZ = [1 1 1 0 0 0 1]; % BPSK - 1001001 DPSK - 1110001
+% EL  = [1 0 0 0 0 0 1]; % BPSK - 1100001 DPSK - 1000001
+
+% [funct, antenna] = ident(data);
 % 
-% for i = 1:length(testArr)
-%     %code for 1 signal
-%     if mod(d, 5) == 0
-%         index = i-1;
-%         %should grab all columns
-%         data2 = data(prevIndex:index, :);
-%         %move all code in here
-%         prevIndex = index;
-%         
-%     elseif testArr(i) ~= testArr(i+1)
-%         d = d + 1;
-%     end
-% end
 % 
-% if(index < testArr)
-% %add code to plot last thing if there is one
-% end
-
-AZ  = [0 0 1 0 0 0 1]; % BPSK - 0011001 DPSK - 0010001
-BAZ = [1 1 1 0 0 0 1]; % BPSK - 1001001 DPSK - 1110001
-EL  = [1 0 0 0 0 0 1]; % BPSK - 1100001 DPSK - 1000001
-
-[funct, antenna] = ident(data);
-
-
-[thetaR, thetaBW, thetaMIN, thetaMAX, AmpT] = modVar(funct, antenna)
-phaseShift
+% [thetaR, thetaBW, thetaMIN, thetaMAX, AmpT] = modVar(funct, antenna)
 
 % figure out how to get T
 % T = 0:1/20:1;
@@ -45,19 +45,10 @@ phaseShift
 
 %mod theta T accordingly
 t = [];
-thetaT = time/ + thetaMax;
-x = ((thetaT-thetaR)/(1.15*thetaBW))./(pi*((thetaT-thetaR)/(1.15*thetaBW)));
-v = AmpT * sinc(x).*sin(omega*T+phaseShift);
-plot(t, v);
+% thetaT = time/ + thetaMax;
+% x = ((thetaT-thetaR)/(1.15*thetaBW))./(pi*((thetaT-thetaR)/(1.15*thetaBW)));
+% v = AmpT * sinc(x).*sin(omega*T+phaseShift);
+% plot(t, v);
 
-thetaT = time/ + thetaMax;
-x = ((thetaT-thetaR)/(1.15*thetaBW))./(pi*((thetaT-thetaR)/(1.15*thetaBW)));
-v = AmpT * sinc(x).*sin(omega*T+phaseShift);
-plot(t, v);
-
-thetaT = time/ + thetaMax;
-x = ((thetaT-thetaR)/(1.15*thetaBW))./(pi*((thetaT-thetaR)/(1.15*thetaBW)));
-v = AmpT * sinc(x).*sin(omega*T+phaseShift);
-plot(t, v);
 end
 
--- test.m
+++ test.m
@@ -3,21 +3,21 @@
 %bpskdpsk(a)
 %[thetaR, thetaBW, thetaMIN, thetaMax, A] = modVar(0010001, 0)
 
-% testArr = [0 0 0 1 0 0 1 0 1 0 0 0 1 0 0 0 0 1 0 0 1 0 1 0 0 0 0 0];
-% d = 0;
-% index = 0;
-% for i = 1:length(testArr)
-%     if d == 7
-%         index = i-1;
-%         break;
-%     elseif testArr(i) ~= testArr(i+1)
-%         d = d + 1;
-%     end
-% end
-% 
-% arr = testArr(1:index)
-% testArr = testArr(index+1:end)
+testArr = [0 0 0 1 0 0 1 0 1 0 0 0 1 0 0 0 0 1 0 0 1 0 1 0 0 0 0 0];
+d = 0;
+index = 0;
+for i = 1:length(testArr)
+    if d == 5
+        index = i-1;
+        break;
+    elseif testArr(i) ~= testArr(i+1)
+        d = d + 1;
+    end
+end
 
-arr1 = [1 0 0 1]
-arr2 = [0 0 1 1]
-arr3 = and(arr1,arr2)
+arr = testArr(1:index)
+testArr = testArr(index+1:end)
+
+% arr1 = [1 0 0 1]
+% arr2 = [0 0 1 1]
+% arr3 = and(arr1,arr2)
