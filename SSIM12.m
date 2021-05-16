ab0b8b4ddb83aae5a64ba6df0cb047c2e34331fd
sbrosh1 <83826020+sbrosh1@users.noreply.github.com>
2021-05-16 17:40:37 -0400

    preamble needs work

--- SSIM12.m
+++ SSIM12.m
@@ -4,46 +4,39 @@
 
 
 
-AZ  = 0010001; % BPSK - 0011001
-BAZ = 1110001; % BPSK - 1001001
-EL  = 1000001; % BPSK - 1100001
-BD1 = 0110000;
-BD2 = 0101000;
-BD3 = 1100000;
-BD4 = 1111000;
-BD5 = 1001000;
-BD6 = 0001001;
-AD1 = 1011100;
-AD2 = 1100101;
-AD3 = 1010000;
-
-functArray(1) = signal(1170,2);
-functArray(2) = signal(1250,2);
-functArray(3) = signal(1310,2);
-functArray(4) = signal(1390,2);
-functArray(5) = signal(1440,2);
-functArray(6) = signal(1500,2);
-functArray(7) = signal(1570,2);
-
-validateattributes(functArray, {'numeric'}, {'integer', 'nonnegative', '<', 10});
-funct = polyval(functArray, 10);
-funct = bpskdpsk(funct);
+AZ  = '0  0  1  0  0  0  1'; % BPSK - 0011001
+BAZ = '1  1  1  0  0  0  1'; % BPSK - 1001001
+EL  = '1  0  0  0  0  0  1'; % BPSK - 1100001
 
 
-a = [1, 1, 0, 1, 1, 0, 1, 1; 1, 0, 1, 0, 0, 0, 1, 0];
 
+
+%a = [1, 1, 0, 1, 1, 0, 1, 1; 1, 0, 1, 0, 0, 0, 1, 0];
+a = totalMatrix;
 
 outArray = zeros(length(a(:, 1)), 1);
 
-omega = 156000;
+thetaR = 0;
+thetaBW = 0;
+thetaMIN = 0;
+thetMax = 0;
+functionID = zeros(7, 1);
+
+omega = 1000;
 
 prevSBS = 0;
 
 preamble = zeros(1, 25);
-preambleItr = 0;
+preambleItr = 1;
+preambleIndex = -1;
+
+scanning = false;
+
+y = 0;
 
 BPSK = a(:, 2);
-DPSK = bpskdpsk(BPSK);
+% DPSK = bpskdpsk(BPSK);
+DPSK = BPSK;
 for i = 1: length(a)  % file hasn't ended yet (400,000+ iterations)
     TX_enable = a(i, 1);
     DPSKitr = DPSK(i);
@@ -58,21 +51,21 @@ for i = 1: length(a)  % file hasn't ended yet (400,000+ iterations)
         % Only check if antenna read is enabl
         
         switch ANT
-            case '0 0 0'
+            case '0  0  0'
                 A = 1;
-            case '0 0 1'
+            case '0  0  1'
                 A = 0.5;
-            case '0 1 0'
+            case '0  1  0'
                 A = 0.25;
-            case '0 1 1'
+            case '0  1  1'
                 A = 0.125;
-            case '1 0 0'
+            case '1  0  0'
                 A = -1; %supposed to be unused
-            case '1 0 1'
+            case '1  0  1'
                 A = 10;
-            case '1 1 0'
+            case '1  1  0'
                 A = 0;
-            case '1 1 1'
+            case '1  1  1'
                 A = 0;
         end
     end
@@ -82,34 +75,70 @@ for i = 1: length(a)  % file hasn't ended yet (400,000+ iterations)
     if TX_enable == 1
         
         %marks start of preamble
-        if preambleIndex ~= -1
+        if preambleIndex == -1
             preambleIndex = i;
         end
         
         if SBS == 1 && prevSBS == 0
+            
+            functionID = preamble(19:end); %the function ID is the last seven bits of the preamble
+            
+            % Check funciton ID. Change angles based on function ID.
+            
+            if strcmp(num2str(functionID), AZ)
+                thetaR = -12;
+                thetaBW = 2;
+                thetaMIN = -62;
+                thetaMAX = 62;
+               
+            elseif strcmp(num2str(functionID), BAZ)
+                thetaR = 12;
+                thetaBW = 2;
+                thetaMIN = -2;
+                thetaMAX = 30;
+            elseif strcmp(num2str(functionID), EL)
+                thetaR = 3;
+                thetaBW = 1.5;
+                thetaMIN = -42;
+                thetaMAX = 42;
+            else
+                thetaR = 0;
+                thetaBW = 0;
+                thetaMIN = 0;
+                thetMax = 0;
+            end
                    
             while TX_enable ~= 0
                 TX_enable = a(i, 1);
                 TO_FRO = a(i, 3);
+                SBS = a(i, 4);
                 
-                % output scanning beacon
+                if TO_FRO == 1 % Scanning TO thetaMIN + t/50
+                    thetaT = thetaMIN + i/50;
+                end
+                if TO_FRO == 0 % Scanning FRO thetaMIN - t/50
+                    thetaT = thetaMIN - i/50;
+                    
+                end
                 
+                outArray(i) = A * (sin(pi*(thetaT - thetaR)/(1.15*thetaBW)) / (pi * (thetaT - thetaR) / (1.15 * thetaBW)) * sin(omega * i));
                 i = i + 1;
             end
+            i = i - 1;
         else
             
             outArray(i) = A * sin(i * omega + DPSKitr * pi);
-            if A == 1 && mod(preambleItr,64) == preambleIndex % if a preamble is currently being outpted, need to cature the function ID
-                preamble(preambleItr) = DPSKitr;
+            if A == 1 && mod(i,64) == mod(preambleIndex,64) % if a preamble is currently being outpted, need to cature the function ID
+                preamble(preambleItr) = DPSKitr
+                preambleItr = preambleItr + 1;
                 
             end
-            preambleItr = preambleItr + 1;
-     
         end
                 
         
     else
         outArray(i) = 0;
+        preambleIndex = -1;
         
     end
     
@@ -117,10 +146,12 @@ for i = 1: length(a)  % file hasn't ended yet (400,000+ iterations)
     
     % if the complete function ID is conveyed reset preamble iterator
     if preambleItr == 25
-        preambleItr = 0;
-        preambelIndex = -1;
+        preambleItr = 1;
+        preambleIndex = -1;
     end
     
     
 end
     
+
+plot(1:length(outArray), outArray)
