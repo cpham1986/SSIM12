c09db17663af164b1dd2f7cc7496a89235d9c937
cpham1986 <chrispham0900@yahoo.com>
2021-05-16 17:48:33 -0400

    asd

--- bpskdpsk64.m
+++ bpskdpsk64.m
@@ -1,24 +1,20 @@
-
 % function: converts an array from BPSK to DPSK
 % input: bpsk - array of BPSK data from TCU
 % output: dpsk - converted BPSK array to DPSK
-function dpsk = bpskdpsk(bpsk)
+function dpsk = bpskdpsk64(bpsk)
 
 dpsk = zeros(1,length(bpsk)); % allocates memory for the new array
 prevVal = 0; % holds value of previous bit
 
 % iterates through the entire array
 for i = 1:length(bpsk)
-    if bpsk(i) == 1
-        dpsk(i) = ~(prevVal); % shifts value 180 degrees
-    else
-        dpsk(i) = prevVal; % retains value of previous bit
+    if mod(i,64)==0
+        if bpsk(i) == 1
+            dpsk(i:i+64) = ~(prevVal); % shifts value 180 degrees
+        else
+            dpsk(i:i+64) = prevVal; % retains value of previous bit
+        end
+        prevVal = dpsk(i); % holds value of previous bit
     end
-    prevVal = dpsk(i); % holds value of previous bit
 end
-
 end
-
-
-
-
